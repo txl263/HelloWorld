ed6a6a9e9d64b7bc97851af25b892f0cc797d3a7
txl263 <txl263@163.com>
2015-01-07 02:49:56 +0800

    new

---
+++ HelloWorld/helloworld.h
@@ -0,0 +1,13 @@
+//
+//  helloworld.h
+//  HelloWorld
+//
+//  Created by Eric Tian on 15/1/7.
+//  Copyright (c) 2015年 Eric Tian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface helloworld : NSObject
+
+@end
---
+++ HelloWorld/helloworld.m
@@ -0,0 +1,13 @@
+//
+//  helloworld.m
+//  HelloWorld
+//
+//  Created by Eric Tian on 15/1/7.
+//  Copyright (c) 2015年 Eric Tian. All rights reserved.
+//
+
+#import "helloworld.h"
+
+@implementation helloworld
+
+@end
